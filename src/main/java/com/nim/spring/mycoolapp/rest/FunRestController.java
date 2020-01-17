f24e07e49a2696c68817d179c4dc58ee576d9631
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2020-01-17 17:55:28 +0000

    Description: - Creates a Hello World endpoint which returns the time on the server

---
+++ src/main/java/com/nim/spring/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.nim.spring.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+    // expose "/" that returns "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is " + LocalDateTime.now();
+    }
+}
