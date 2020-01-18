4121e53e600331629f49b7d03c49d08938ee14ce
Nimalan Kirubakaran <nimalankirubakaran@Nimalans-MacBook-Pro.local>
2020-01-18 10:17:45 +0000

    Added different servlet context and port number

--- src/main/java/com/nim/spring/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/nim/spring/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.nim.spring.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,17 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
     // expose "/" that returns "Hello World"
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
